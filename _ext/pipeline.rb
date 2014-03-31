c422da76afc7dd4a5c4aeeddb472355a805b5290
jugonzal <jugonzal@redhat.com>
2014-03-31 16:36:11 +0200

    Changes in Team Page and more

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -3,4 +3,8 @@ require File.expand_path '../haml/filters/asciidoc.rb', __FILE__
 Awestruct::Extensions::Pipeline.new do
 	helper Awestruct::Extensions::Partial
 	helper Awestruct::Extensions::Relative
+	extension Awestruct::Extensions::DataDir.new
+	extension Awestruct::Extensions::Posts.new( '/news', :posts )
+	extension Awestruct::Extensions::Paginator.new(:posts, '/news/index', :per_page => 5 )
 end
+
