0c11ca41326e726396fc97a978964441b2c12207
jugonzal <jugonzal@redhat.com>
2014-04-02 12:44:33 +0200

    Changes in principal menu

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,10 +1,17 @@
 require File.expand_path '../haml/filters/asciidoc.rb', __FILE__
 
 Awestruct::Extensions::Pipeline.new do
-	helper Awestruct::Extensions::Partial
-	helper Awestruct::Extensions::Relative
-	extension Awestruct::Extensions::DataDir.new
-	extension Awestruct::Extensions::Posts.new( '/news', :posts )
-	extension Awestruct::Extensions::Paginator.new(:posts, '/news/index', :per_page => 5 )
+        extension Awestruct::Extensions::DataDir.new
+        extension Awestruct::Extensions::Posts.new( '/news', :posts )
+        extension Awestruct::Extensions::Paginator.new(:posts, '/news/index', :per_page => 5 )
+	extension Awestruct::Extensions::Disqus.new
+        
+        # Indexifier moves HTML files to their own directory to achieve "pretty" URLs (e.g., features.html -> /features/index.html)
+        #extension Awestruct::Extensions::Indexifier.new
+
+        # Helpers
+        helper Awestruct::Extensions::Partial
+        helper Awestruct::Extensions::Relative
+
 end
 
